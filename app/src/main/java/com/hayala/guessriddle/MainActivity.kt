1b2e82c6e8ed24738744483933dc001afd334d6a
hayala124 <elnur7803@gmail.com>
2024-05-14 20:38:45 +0300

    работает половина от общего задания

--- app/src/main/java/com/hayala/guessriddle/MainActivity.kt
+++ app/src/main/java/com/hayala/guessriddle/MainActivity.kt
@@ -1,24 +1,134 @@
 package com.hayala.guessriddle
 
 import android.content.Intent
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.View
+import android.widget.TextView
 import com.hayala.guessriddle.databinding.ActivityMainBinding
+import kotlinx.parcelize.Parcelize
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    // private var listOfRiddles = listOf("Не лает, не кусает, а в дом не пускает", "В огне не горит, в воде не тонет", "Что всегда идёт, а с места не сойдёт?", "Какое слово всегда пишется неправильно?", "На столе лежали три огурца и четыре яблока. Ребенок взял со стола одно яблоко. Сколько фруктов осталось на столе?")
+    //, "", "", "", "", "", "", "", "")
+
+
+    private val mapOfRiddlesAndAnswers = mapOf(
+        "замок" to "Не лает, не кусает, а в дом не пускает?",
+        "лёд" to "В огне не горит, в воде не тонет?",
+        "часы" to "Что всегда идёт, а с места не сойдёт?",
+        "неправильно" to "Какое слово всегда пишется неправильно?",
+        "3" to "На столе лежали три огурца и четыре яблока. Ребенок взял со стола одно яблоко. Сколько фруктов осталось на столе?",
+        "1" to "Сколько раз из числа 1111 можно вычесть 1?"
+    )
+
+    /* "" to "",
+     "" to "",
+     "" to "",
+     "" to "",
+     "" to "",
+     "" to "",
+     "" to "",
+     "" to "",
+     "" to ""*/
+    private lateinit var textViewRiddle: TextView
+    private lateinit var riddle: String
+
+    lateinit var state: State
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        textViewRiddle = binding.textRiddle
 
-        binding.btnOpenRiddle.setOnClickListener {openRiddleActivity()}
+        binding.btnOpenRiddle.setOnClickListener { openRiddleActivity() }
+        binding.btnExit.setOnClickListener { exitApplication() }
+        binding.btnAnswer.setOnClickListener { giveAnswerToRiddle() }
+
+        state = if (savedInstanceState == null) {
+            State(
+                buttonStatisticsIsEnabled = false,
+                buttonOpenRiddleIsEnabled = true,
+                buttonAnswerIsEnabled = false,
+                buttonExitIsEnabled = false,
+                buttonStartOverIsEnabled = false,
+                buttonStatisticsColor = getColor(R.color.gray),
+                buttonOpenRiddleColor = getColor(R.color.pink),
+                buttonAnswerColor = getColor(R.color.gray),
+                buttonExitColor = getColor(R.color.gray),
+                buttonStartOverColor = getColor(R.color.gray),
+                countOfSolvedRiddles = 0
+            )
+        } else {
+            savedInstanceState.getParcelable(KEY_STATE)!!
+        }
+        saveState()
     }
 
     private fun openRiddleActivity() {
+        //var key =
+        // val randomKey =
+        riddle = mapOfRiddlesAndAnswers[mapOfRiddlesAndAnswers.keys.random()].toString()
+        textViewRiddle.text = riddle
+        state.buttonOpenRiddleIsEnabled = !state.buttonOpenRiddleIsEnabled
+        state.buttonOpenRiddleColor = getColor(R.color.gray)
+        state.buttonAnswerIsEnabled = !state.buttonAnswerIsEnabled
+        state.buttonAnswerColor = getColor(R.color.pink)
+        saveState()
+    }
+
+    private fun giveAnswerToRiddle() {
+        //saveState()
         val intent = Intent(this, RiddleActivity::class.java)
+        intent.putExtra("numberPageOne", state.countOfSolvedRiddles.toString())
         startActivity(intent)
+    }
+
+    private fun exitApplication() {
+        finish()
+    }
+
+    private fun saveState() = with(binding) {
+        btnStatistics.isEnabled = if (state.buttonStatisticsIsEnabled) true else false
+        btnOpenRiddle.isEnabled = if (state.buttonOpenRiddleIsEnabled) true else false
+        btnAnswer.isEnabled = if (state.buttonAnswerIsEnabled) true else false
+        btnExit.isEnabled = if (state.buttonExitIsEnabled) true else false
+        btnStartOver.isEnabled = if (state.buttonStartOverIsEnabled) true else false
+        btnStatistics.backgroundTintList = (ColorStateList.valueOf(state.buttonStatisticsColor))
+        btnOpenRiddle.backgroundTintList = (ColorStateList.valueOf(state.buttonOpenRiddleColor))
+        btnAnswer.backgroundTintList = (ColorStateList.valueOf(state.buttonAnswerColor))
+        btnExit.backgroundTintList = (ColorStateList.valueOf(state.buttonExitColor))
+        btnStartOver.backgroundTintList = (ColorStateList.valueOf(state.buttonStartOverColor))
+        textCountAnswers.setText(state.countOfSolvedRiddles.toString())
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(KEY_STATE, state)
+    }
+
+    @Parcelize
+    class State(
+        var buttonStatisticsIsEnabled: Boolean,
+        var buttonOpenRiddleIsEnabled: Boolean,
+        var buttonAnswerIsEnabled: Boolean,
+        var buttonExitIsEnabled: Boolean,
+        var buttonStartOverIsEnabled: Boolean,
+        var buttonStatisticsColor: Int,
+        var buttonOpenRiddleColor: Int,
+        var buttonAnswerColor: Int,
+        var buttonExitColor: Int,
+        var buttonStartOverColor: Int,
+        var countOfSolvedRiddles: Int
+    ) : Parcelable
+
+    companion object {
+        @JvmStatic
+        private val KEY_STATE = "STATE"
     }
 }
--- app/src/main/java/com/hayala/guessriddle/RiddleActivity.kt
+++ app/src/main/java/com/hayala/guessriddle/RiddleActivity.kt
@@ -1,19 +1,126 @@
 package com.hayala.guessriddle
 
+import android.content.Intent
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import com.hayala.guessriddle.databinding.ActivityMainBinding
 import com.hayala.guessriddle.databinding.ActivityRiddleBinding
+import kotlinx.parcelize.Parcelize
+import java.lang.StrictMath.random
+import kotlin.properties.Delegates
+import kotlin.random.Random
 
 class RiddleActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRiddleBinding
-    private var listOfRiddles = listOf("Не лает, не кусает, а в дом не пускает", "В огне не горит, в воде не тонет", "Что всегда идёт, а с места не сойдёет?", "", "", "", "", "", "", "", "", "", "")
-    private var listOfAnswers = listOf("Замок", "лёд", "часы", "", "", "", "", "", "", "", "", "", "", "", "")
+
+    private var listOfAnswers =
+        listOf("замок", "лёд", "часы", "неправильно", "3", "1")
+    //, "", "", "", "", "", "", "", "", "")
+
+    private var count by Delegates.notNull<Int>()
+    lateinit var state: State
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRiddleBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnCheck.setOnClickListener { buttonCheckPressed() }
+        val dataOne = intent.getStringExtra("numberPageOne")
+        binding.textView1.text = dataOne
+        count = dataOne.toString().toInt() + 1
 
+        val shuffledList = listOfAnswers.shuffled()
+
+        // Шаг 3: Создание нового списка и копирование перемешанных элементов
+        val newList = mutableListOf<String>()
+        shuffledList.forEach { newList.add(it) }
+
+        // Вывод нового списка
+        println("New List: $newList")
+
+        val radioGroup = binding.radioGroupAnswers
+        for (option in newList) {
+            val radioButton = RadioButton(this)
+            radioButton.text = option
+            radioGroup.addView(radioButton)
+        }
+
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            val selectedButton = findViewById<RadioButton>(checkedId)
+            state.buttonCheckColor = getColor(R.color.pink)
+            state.buttonCheckIsEnabled = true
+            binding.btnCheck.isEnabled = state.buttonCheckIsEnabled
+
+            var intent2 = Intent(this, MainActivity::class.java)
+
+            saveState()
+        }
+
+        /*binding.radioGroupAnswers.setOnCheckedChangeListener { group, checkedId ->
+            // Проверяем, была ли выбрана какая-либо радиокнопка
+            if (checkedId != -1) {
+                state.buttonCheckIsEnabled = !state.buttonCheckIsEnabled
+                state.buttonCheckColor = getColor(R.color.pink)
+            }
+            saveState()
+        }*/
+
+        state = if (savedInstanceState == null) {
+            State(
+                buttonCheckIsEnabled = false,
+                buttonCheckColor = getColor(R.color.gray)
+            )
+        } else {
+            savedInstanceState.getParcelable(KEY_STATE)!!
+        }
+
+        saveState()
+    }
+
+    /*fun onRadioButtonClicked(view: View) {
+        // Получаем ID выбранной радиокнопки
+        val radioButton = view as RadioButton
+        val button = findViewById<Button>(R.id.button)
+
+        // Активируем кнопку отправки, если была выбрана какая-либо радиокнопка
+        if (radioButton.isChecked) {
+            button.isEnabled = true
+        } else {
+            button.isEnabled = false
+        }
+    }*/
+
+    fun buttonCheckPressed() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("numberPageTwo", count.toString())
+        startActivity(intent)
+        saveState()
+    }
+
+    private fun saveState() = with(binding) {
+        btnCheck.isEnabled = if (state.buttonCheckIsEnabled) true else false
+        btnCheck.backgroundTintList = ColorStateList.valueOf(state.buttonCheckColor)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(KEY_STATE, state)
+    }
+
+    @Parcelize
+    class State(
+        var buttonCheckIsEnabled: Boolean,
+        var buttonCheckColor: Int,
+    ) : Parcelable
+
+    companion object {
+        @JvmStatic
+        private val KEY_STATE = "STATE"
     }
 }
